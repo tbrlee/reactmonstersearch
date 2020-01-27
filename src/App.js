684ea030dacf20130521885ada6db33ac4ab6c96
Brian Lee <tzhlee@gmail.com>
2020-01-27 04:00:04 -0600

    State monsters2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -23,6 +22,13 @@ class App extends Component {
 			]
 		};
 	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((users) => console.log(users));
+	}
+
 	render() {
 		return (
 			<div className="App">{this.state.monsters.map((monster) => <h1 key={monster.id}>{monster.name}</h1>)}</div>
