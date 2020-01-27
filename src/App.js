813cd2c44a2863ff5c4d8f97c00dfbbb9073de1d
Brian Lee <tzhlee@gmail.com>
2020-01-27 10:31:56 -0600

    API state name lists

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 	componentDidMount() {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then((response) => response.json())
-			.then((users) => console.log(users));
+			.then((users) => this.setState({ monsters: users }));
 	}
 
 	render() {
