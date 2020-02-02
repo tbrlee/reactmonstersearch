8e0e234d165065c13cbf73cf749fe4cdd2031010
Brian Lee <tzhlee@gmail.com>
2020-02-02 01:26:31 -0600

    monster image ontained from URL and implemented in the card component

---
+++ src/components/card-list/card-list.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from '../card/card.component';
+import './card-list.styles.css';
+
+const CardList = (props) => (
+	//console.log(props);
+	//return <div className="card-list">{props.children}</div>;
+	<div className="card-list">{props.monsters.map((monster) => <Card key={monster.id} monster={monster} />)}</div>
+);
+export default CardList;
