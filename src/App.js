7b375cd4d33cf3edd2d7dfab6fb5a3f470ffcc3b
Brian Lee <tzhlee@gmail.com>
2020-02-03 15:55:37 -0600

    Seachfield state created  and real time search completed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
 		super();
 
 		this.state = {
-			monsters: []
+			monsters: [],
+			searchField: ''
 		};
 	}
 
@@ -18,9 +19,23 @@ class App extends Component {
 	}
 
 	render() {
+		/* Destructring equivalent
+		const monsters = this.state.monsters;
+		const searchField = this.state.searchField
+		*/
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
+
 		return (
 			<div className="App">
-				<CardList monsters={this.state.monsters} />
+				<input
+					type="search"
+					placeholder="search monsters"
+					onChange={(e) => this.setState({ searchField: e.target.value })}
+				/>
+				<CardList monsters={filteredMonsters} />
 			</div>
 		);
 	}
