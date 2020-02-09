37eb2e0841aef415e5f6dcaaf96d7ad8680ddea4
Brian Lee <tzhlee@gmail.com>
2020-02-08 23:56:31 -0600

    Updates

---
+++ precache-manifest.6101a8477f6a39dbe7d0e310eb6e0eb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920e36a761c3858e009d7bfccf6ae35a",
+    "url": "/reactmonstersearch/index.html"
+  },
+  {
+    "revision": "f3fc4a8ac8fccc1507eb",
+    "url": "/reactmonstersearch/static/css/main.0bcfdbff.chunk.css"
+  },
+  {
+    "revision": "28f9e3651a909cc1d8e2",
+    "url": "/reactmonstersearch/static/js/2.8edc2ed5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactmonstersearch/static/js/2.8edc2ed5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3fc4a8ac8fccc1507eb",
+    "url": "/reactmonstersearch/static/js/main.84cbfbd5.chunk.js"
+  },
+  {
+    "revision": "7a105a73ad9ec1ec122c",
+    "url": "/reactmonstersearch/static/js/runtime-main.095e3c4f.js"
+  }
+]);
